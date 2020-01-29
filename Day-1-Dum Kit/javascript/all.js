52d2794d328f1a78cc4113f166e217ce6add7ec0
purple1007 <purple1007@gmail.com>
2020-01-29 15:49:04 +0800

    add playsound js

--- Day-1-Dum Kit/javascript/all.js
+++ Day-1-Dum Kit/javascript/all.js
@@ -1,3 +1,24 @@
-window.addEventListener('keydown',function(e) {
-  console.log(e.keyCode);
-});
+function playSound(e) {
+   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   const keybox = document.querySelector(`div.keybox[data-key="${e.keyCode}"]`);
+   if(!audio) return;
+   // console.log(e.keyCode);
+   // console.log(audio);
+   // console.log(keybox);
+   audio.currentTime = 0;
+   audio.play();
+   keybox.classList.add('active');
+};
+
+function removeTransition(e){
+  // console.log(e);
+  if(e.propertyName !== 'color') return;
+  console.log(e.propertyName);
+  // console.log(this);
+  this.classList.remove('active');
+};
+
+const keyboxs = document.querySelectorAll('.keybox');
+keyboxs.forEach(keybox => keybox.addEventListener('transitionend',removeTransition));
+
+window.addEventListener('keydown',playSound);
